5cc90e9513c8908394992ef731a8cfa5ed797d32
kauanuness <kaua.souza@sptech.school>
2023-11-24 19:47:19 -0300

    comentando meu código e limpando ele

--- site/public/js/cadastro.js
+++ site/public/js/cadastro.js
@@ -1,74 +1,64 @@
-function ButtonCadastrar(){
+function ButtonCadastrar() {
+  // Recuperar valores dos campos de entrada
+  var emailVar = input_email.value;
+  console.log(emailVar);
+  var nomeVar = input_nome.value;
+  console.log(nomeVar);
+  var senhaVar = input_senha.value;
+  console.log(senhaVar);
+  var repetirSenhaVar = input_repetir_senha.value;
+  console.log(repetirSenhaVar);
+  var personagemVar = select_personagem.value;
+  console.log(personagemVar);
 
-    var emailVar = input_email.value;
-    console.log(emailVar)
-    var nomeVar = input_nome.value;
-    console.log(nomeVar)
-    var senhaVar = input_senha.value;
-    console.log(senhaVar)
-    var repetirSenhaVar = input_repetir_senha.value;
-    console.log(repetirSenhaVar)
-    var personagemVar = select_personagem.value;
-    console.log(personagemVar)
-
-    //Recupere o valor da nova input pelo nome do id
-    // Agora vá para o método fetch logo abaixo
-    
-    if (
+  // Verificar se algum campo está em branco
+  if (
       emailVar == "" ||
       senhaVar == "" ||
       repetirSenhaVar == "" ||
       personagemVar == "" ||
       nomeVar == ""
-    ) {
- 
-      alert("Mensagem de erro: Campos em branco")
-
+  ) {
+      alert("Mensagem de erro: Campos em branco");
       return false;
-    } else {
-      setInterval( 5000);
-    }
+  } else {
+      // A função setInterval não parece ter um propósito claro aqui.
+      // Se não for necessário, você pode removê-la.
+      // setInterval(5000);
+  }
 
-    // Enviando o valor da nova input
-    fetch("/usuarios/cadastrar", {
+  // Enviar dados para o servidor usando o método fetch
+  fetch("/usuarios/cadastrar", {
       method: "POST",
       headers: {
-        "Content-Type": "application/json",
+          "Content-Type": "application/json",
       },
       body: JSON.stringify({
-        // crie um atributo que recebe o valor recuperado aqui
-        // Agora vá para o arquivo routes/usuario.js
-        nomeServer: nomeVar,
-        emailServer: emailVar,
-        senhaServer: senhaVar,
-        repetirSenhaServer: repetirSenhaVar,
-        personagemServer: personagemVar
+          nomeServer: nomeVar,
+          emailServer: emailVar,
+          senhaServer: senhaVar,
+          repetirSenhaServer: repetirSenhaVar,
+          personagemServer: personagemVar,
       }),
-    })
+  })
       .then(function (resposta) {
-        console.log("resposta: ", resposta);
+          console.log("resposta: ", resposta);
 
-        if (resposta.ok) {
-        //   cardErro.style.display = "block";
+          if (resposta.ok) {
+              // Exibir mensagem de sucesso
+              alert("Cadastro Realizado com sucesso");
 
-        //   mensagem_erro.innerHTML =
-        //     "Cadastro realizado com sucesso! Redirecionando para tela de Login...";
-          alert("Cadastro Realizado com sucesso");
-
-          setTimeout(() => {
-            window.location.href = "login.html";
-          }, "1000");
-
-          
-          
-        } else {
-          throw "Houve um erro ao tentar realizar o cadastro!";
-        }
+              // Redirecionar para a página de login após um breve intervalo
+              setTimeout(() => {
+                  window.location.href = "login.html";
+              }, 1000);
+          } else {
+              throw "Houve um erro ao tentar realizar o cadastro!";
+          }
       })
       .catch(function (resposta) {
-        console.log(`#ERRO: ${resposta}`);
-        
+          console.log(`#ERRO: ${resposta}`);
       });
 
-    return false;
-  }
+  return false; // Evitar o envio do formulário tradicional (se aplicável)
+}
