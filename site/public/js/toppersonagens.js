a7ade34c94ae7da1d4a7dfe209c4dc1446d882b1
kauanuness <kaua.souza@sptech.school>
2023-11-25 09:07:05 -0300

    explicando meu código e organizando

--- site/public/js/toppersonagens.js
+++ site/public/js/toppersonagens.js
@@ -1,87 +1,95 @@
+// Fazendo uma solicitação GET para "/usuarios/grafico"
 fetch("/usuarios/grafico", {
     method: "GET",
     headers: {
         "Content-Type": "application/json"
     },
-
-}).then(function (resposta) {
-
-    if (resposta.ok) {
-        console.log(resposta);
-
-        resposta.json().then(json => {
-            console.log(json);
-            console.log(JSON.stringify(json));
-
-            const ctx = document.getElementById('myChart');
-
-            var labelctx = json.map(function(item){
-                return item.nome
-
-            });
-
-            var datasetsctx = json.map(function(item){
-                return item.count
-
-            });
-
-            new Chart(ctx, {
-                type: 'doughnut',
-                data: {
-                    labels: labelctx,
-                    datasets: [{
-                        label: '# of Votes',
-                        data: datasetsctx,
-                        borderWidth: 1
-                    }]
-                },
-                options: {
-                    scales: {
-                        y: {
-                            beginAtZero: true
-                        }
-                    }
-                }
-            });
-
-            var datasetsctx2 = json.map(function (item) {
-                return {
-                    label: item.nome,
-                    data: [item.count],
-                    borderWidth: 1
-                }
-            })
-
-            const ctx2 = document.getElementById('myChart2');
-            new Chart(ctx2, {
-                type: 'bar',
-                data: {
-                    labels: ['Mais Escolhidos como Personagens Favoritos'],
-                    datasets: datasetsctx2
-
-                },
-
-                options: {
-                    scales: {
-                        y: {
-                            beginAtZero: true
-                        }
-                    }
-                }
-            });
-
-
-        });
-
-    } else {
-
-        console.log("Houve um erro ao tentar realizar o login!");
-
-        resposta.text().then(texto => {
-            console.error(texto);
-        });
-    }
-
-}).catch(function (erro) {
-    console.log(erro);
 })
+    // Manipulando a resposta da solicitação
+    .then(function (resposta) {
+        // Verificar se a resposta foi bem-sucedida
+        if (resposta.ok) {
+            console.log(resposta);
+
+            // Convertendo a resposta para JSON
+            resposta.json().then(json => {
+                console.log(json);
+                console.log(JSON.stringify(json));
+
+                // Configurando o primeiro gráfico de rosquinha (doughnut)
+                const ctx = document.getElementById('myChart');
+                var labelctx = json.map(function (item) {
+                    return item.nome;
+                });
+
+                // é uma função de array em JavaScript que cria um novo array com os resultados//
+                // de chamar uma função fornecida para cada elemento no array original//
+                
+                // neste caso, json é um array de objetos. A função passada para map extrai a propriedade nome de cada objeto no array,//
+                // criando um novo array contendo apenas os valores da propriedade nome.//
+
+                // optei por usar o método map em vez de um loop for para transformar elementos do array de forma mais concisa, //
+                // expressiva e funcional, promovendo uma leitura simplificada e favorecendo a imutabilidade do array original.//
+
+                var datasetsctx = json.map(function (item) {
+                    return item.count;
+                });
+
+                new Chart(ctx, {
+                    type: 'doughnut',
+                    data: {
+                        labels: labelctx,
+                        datasets: [{
+                            label: '# of Votes',
+                            data: datasetsctx,
+                            borderWidth: 1
+                        }]
+                    },
+                    options: {
+                        scales: {
+                            y: {
+                                beginAtZero: true
+                            }
+                        }
+                    }
+                });
+
+                // Configurando o segundo gráfico de barras (bar)
+                var datasetsctx2 = json.map(function (item) {
+                    return {
+                        label: item.nome,
+                        data: [item.count],
+                        borderWidth: 1
+                    }
+                })
+
+                const ctx2 = document.getElementById('myChart2');
+                new Chart(ctx2, {
+                    type: 'bar',
+                    data: {
+                        labels: ['Mais Escolhidos como Personagens Favoritos'],
+                        datasets: datasetsctx2
+                    },
+                    options: {
+                        scales: {
+                            y: {
+                                beginAtZero: true
+                            }
+                        }
+                    }
+                });
+            });
+
+        } else {
+            // Tratar erros se a resposta não for bem-sucedida
+            console.log("Houve um erro ao tentar realizar a solicitação!");
+
+            resposta.text().then(texto => {
+                console.error(texto);
+            });
+        }
+    })
+    // Tratar erros durante a solicitação ou processamento
+    .catch(function (erro) {
+        console.log(erro);
+    });
